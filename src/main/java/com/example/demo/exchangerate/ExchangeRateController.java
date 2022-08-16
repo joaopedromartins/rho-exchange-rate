9bff3e4ae893345cc4bbc198d06ae9b46f82dcee
joaopedromartins <joao.pedro.brandao.martins@gmail.com>
2022-08-16 18:22:15 +0100

    HelloWord

---
+++ src/main/java/com/example/demo/exchangerate/ExchangeRateController.java
@@ -0,0 +1,24 @@
+package com.example.demo.exchangerate;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/v1")
+public class ExchangeRateController {
+    private final ExchangeRateService exchangeRateService;
+
+    @Autowired
+    public ExchangeRateController(ExchangeRateService exchangeRateService) {
+        this.exchangeRateService = exchangeRateService;
+    }
+
+
+    @GetMapping
+    public String getHello() {
+        return exchangeRateService.getHello();
+    }
+
+}
---
+++ src/main/java/com/example/demo/exchangerate/ExchangeRateService.java
@@ -0,0 +1,10 @@
+package com.example.demo.exchangerate;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ExchangeRateService {
+    public String getHello() {
+        return "Hello World";
+    }
+}
